e68a3b9c94c60c6ace81c4e13a2a090e1680e5b7
FelipeM1 <felipemedeirosh@gmail.com>
2022-12-16 15:03:12 -0300

    commiting changes

--- Copygram/Controller/ViewController.swift
+++ Copygram/Controller/ViewController.swift
@@ -10,15 +10,19 @@ import UIKit
 class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource,UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
     
     @IBOutlet weak var hzTableView: UITableView!
-    @IBOutlet weak var tabBar: UITabBar!
+    @IBOutlet weak var tabBarVW: UIView!
     @IBOutlet weak var tabBarViewHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var perfilCollection: UICollectionView!
     @IBOutlet weak var perfilCollectionHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var addBtn: UIButton!
     @IBOutlet weak var notificationsBtn: UIButton!
     @IBOutlet weak var chatBtn: UIButton!
+    @IBOutlet weak var homeBtn: UIButton!
+    @IBOutlet weak var searchBtn: UIButton!
+    @IBOutlet weak var profileBtn: UIButton!
     
     private var lastContentOffset: CGFloat = 0
+    var hasSeen = [false, false, false, false]
     
     override func viewDidLoad() {
         
@@ -36,12 +40,14 @@ class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSourc
     //MARK: CollectionView
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        10
+        hasSeen.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell: PerfilCollectionViewCell? = collectionView.dequeueReusableCell(withReuseIdentifier: "PerfilCollectionViewCell", for: indexPath) as? PerfilCollectionViewCell
         
+        cell?.hasSeen = hasSeen[indexPath.row]
+        cell?.setupUI()
         return cell ?? UICollectionViewCell()
     }
     
@@ -58,9 +64,12 @@ class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSourc
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("tap")
-        (collectionView.cellForItem(at: indexPath) as? PerfilCollectionViewCell)?.hasSeen = true
+        if collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: PerfilCollectionViewCell.self), for: indexPath) is PerfilCollectionViewCell{
+            hasSeen[indexPath.row] =  true
+            collectionView.reloadData()
+        }
     }
+    
     
     //MARK: TableView
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -82,24 +91,21 @@ class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSourc
         return 601
     }
     
+    //MARK: Scroll
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         if scrollView.tag == 1{
             perfilCollectionHeightConstraint.constant = (scrollView.contentOffset.y < 40) ? 90: 0
             
             if (self.lastContentOffset > scrollView.contentOffset.y) {
-                self.tabBarViewHeightConstraint.constant = 80
+                self.tabBarViewHeightConstraint.constant = 70
             } else if (self.lastContentOffset < scrollView.contentOffset.y) {
                 self.tabBarViewHeightConstraint.constant = 0
             }
-            
+            self.lastContentOffset = scrollView.contentOffset.y
             UIView.animate(withDuration: 0.2) {
                 self.view.layoutIfNeeded()
             }
-            self.lastContentOffset = scrollView.contentOffset.y
         }
-        
     }
-    
 }
-
 
--- Copygram/View/Stories/PerfilCollectionViewCell.swift
+++ Copygram/View/Stories/PerfilCollectionViewCell.swift
@@ -13,10 +13,24 @@ class PerfilCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var label: UILabel!
     
-    var hasSeen = false{
-        didSet{
-            setupUI()
-        }
+    var hasSeen = false
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        let gradient = CAGradientLayer()
+        gradient.frame =  CGRect(origin: CGPoint.zero, size: self.borderVW.frame.size)
+        gradient.colors =  [UIColor.yellow.cgColor, UIColor.orange.cgColor, UIColor.systemPink.cgColor, UIColor.purple.cgColor]
+        
+        let shape = CAShapeLayer()
+        shape.lineWidth = 6
+        
+        shape.path = UIBezierPath(roundedRect: self.borderVW.bounds, cornerRadius: self.borderVW.layer.cornerRadius).cgPath
+        
+        shape.strokeColor = UIColor.white.cgColor
+        shape.fillColor = UIColor.clear.cgColor
+        gradient.mask = shape
+        
+        self.borderVW.layer.addSublayer(gradient)
     }
     
     override func awakeFromNib() {
@@ -28,34 +42,30 @@ class PerfilCollectionViewCell: UICollectionViewCell {
         self.borderVW.layer.cornerRadius = self.borderVW.frame.height/2
         self.borderVW.clipsToBounds = true
         label.text = "@seacella._"
-        setupUI()
-    }
 
-//    override func prepareForReuse() {
-//        super.prepareForReuse()
-//        hasSeen = false
-//    }
-    
+    }
     func setupUI() {
-        
+ 
         if hasSeen == false{
             let gradient = CAGradientLayer()
             gradient.frame =  CGRect(origin: CGPoint.zero, size: self.borderVW.frame.size)
             gradient.colors =  [UIColor.yellow.cgColor, UIColor.orange.cgColor, UIColor.systemPink.cgColor, UIColor.purple.cgColor]
-            
+
             let shape = CAShapeLayer()
             shape.lineWidth = 6
-            
+
             shape.path = UIBezierPath(roundedRect: self.borderVW.bounds, cornerRadius: self.borderVW.layer.cornerRadius).cgPath
-            
+
             shape.strokeColor = UIColor.white.cgColor
             shape.fillColor = UIColor.clear.cgColor
             gradient.mask = shape
-            
+
             self.borderVW.layer.addSublayer(gradient)
         }else{
             borderVW.layer.borderWidth = 2
             borderVW.layer.borderColor = UIColor.gray.cgColor
         }
+        
     }
+
 }
